e6976723d733e5e1f1aa3b47de8bfef5b57d89b0
V4lstein <valentin.cuteanu@gmail.com>
2019-11-05 19:30:31 +0200

    Init commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const request = require("request");
+const cheerio = require("cheerio");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/:id/trailer", (req, res) => {
+  const id = req.params.id;
+  const url = `http://www.imdb.com/title/${id}`;
+
+  request(url, (error, response, html) => {
+    if (error) return res.status(500).send({ error: "Something went wrong" });
+
+    const $ = cheerio.load(html);
+
+    const trailerElement = $(".slate_button.prevent-ad-overlay.video-modal");
+    if (trailerElement.children().length) {
+      const trailerID = trailerElement.first().attr()["data-video"];
+      return res.send({
+        id,
+        trailerID,
+        embed: `https://www.imdb.com/videoembed/${trailerID}`
+      });
+    }
+
+    res.status(404).send({ error: "Inexistent ID" });
+  });
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
