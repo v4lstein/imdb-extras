42f17c8365396eaf8993245997ee4b6c44225430
V4lstein <valentin.cuteanu@gmail.com>
2019-11-14 03:40:24 +0200

    Add poster endpoint

---
+++ src/helpers/scrape.js
@@ -0,0 +1,22 @@
+const request = require("request");
+const cheerio = require("cheerio");
+
+const scrape = (url, elementSelector) => {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, html) => {
+      if (error) return reject("An error occurred while scraping");
+
+      const $ = cheerio.load(html);
+
+      const element = $(elementSelector);
+
+      if (element.attr()) {
+        return resolve(element);
+      }
+      element.error = true;
+      return resolve(element);
+    });
+  });
+};
+
+module.exports = scrape;
--- src/index.js
+++ src/index.js
@@ -1,32 +1,63 @@
 const express = require("express");
-const request = require("request");
-const cheerio = require("cheerio");
-
+const scrape = require("./helpers/scrape");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/:id/trailer", (req, res) => {
+// Set response headers
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Content-Type", "application/json");
+  next();
+});
+
+app.get("/:id/trailer", async (req, res) => {
   const id = req.params.id;
   const url = `http://www.imdb.com/title/${id}`;
-  res.setHeader("Content-Type", "application/json");
-  res.setHeader("Access-Control-Allow-Origin", "*");
 
-  request(url, (error, response, html) => {
-    if (error) return res.status(500).send({ error: "Something went wrong" });
+  let trailerElement;
 
-    const $ = cheerio.load(html);
+  try {
+    trailerElement = await scrape(
+      url,
+      ".slate_button.prevent-ad-overlay.video-modal"
+    );
+  } catch (e) {
+    return res.status(500).send("Something went wrong");
+  }
 
-    const trailerElement = $(".slate_button.prevent-ad-overlay.video-modal");
-    if (trailerElement.children().length) {
-      const trailerID = trailerElement.first().attr()["data-video"];
-      return res.send({
-        id,
-        trailerID,
-        embed: `https://www.imdb.com/videoembed/${trailerID}`
-      });
-    }
+  if (trailerElement.error) {
+    return res.status(404).send({ error: "No trailer available" });
+  }
 
-    res.status(404).send({ error: "No trailer available" });
+  const trailerID = trailerElement.first().attr()["data-video"];
+  return res.send({
+    id,
+    trailerID,
+    embed: `https://www.imdb.com/videoembed/${trailerID}`
+  });
+});
+
+app.get("/:id/poster", async (req, res) => {
+  const id = req.params.id;
+  const url = `http://www.imdb.com/title/${id}`;
+  let posterElement;
+
+  try {
+    posterElement = await scrape(url, ".poster img");
+  } catch (e) {
+    return res.status(500).send("Something went wrong");
+  }
+
+  if (posterElement.error) {
+    return res.status(404).send({ error: "No poster available" });
+  }
+  const elementAttributes = posterElement.first().attr();
+  const posterLocation = elementAttributes["src"];
+  const posterAlt = elementAttributes["alt"];
+  return res.send({
+    id,
+    posterAlt,
+    posterLocation
   });
 });
 
